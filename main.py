9ff704b8ca3cdf46e4541e0fd8fcdfbd540795a8
Jafeth Garcia <jafi666@hotmail.com>
2014-11-24 19:27:32 -0400

    refactoring to meet standards

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ Created on 21/11/2014
 import sys
 
 from PyQt4 import QtGui
-from pyCommander import Ui_MainWindow
+from views.commander_window import CommanderWindow
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    window = Ui_MainWindow()
+    window = CommanderWindow()
     window.show()
     sys.exit(app.exec_())
 
